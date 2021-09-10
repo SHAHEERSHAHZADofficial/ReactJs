7156ef148c50058a40a0659029063891592905bb
SHAHEER SHAHZAD OFFICIAL <85387726+SHAHEERSHAHZADofficial@users.noreply.github.com>
2021-09-10 20:10:54 +0500

    hello world program

---
+++ myapp/src/component/greeting.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Greeting(){
+       return(
+        <div>
+            <img src="./" />
+  <h1>Hello world</h1>
+  <p>The quick brown fox jump over the lazy dog</p>
+       </div>
+
+);
+
+};
+export default Greeting;
